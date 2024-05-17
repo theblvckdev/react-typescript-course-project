7ecf5aa7c2da86248720b70caf04fa77dda958cd
theblvckdev <nathancodes05@gmail.com>
2024-05-18 00:16:17 +0100

    refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Banner from "./components/banner";
-import TodoContainer from "./components/todo_container";
 
 const App = () => {
   return (
     <>
-      <main className="bg-gray-100 h-screen">
-        <Banner />
-        <TodoContainer />
-      </main>
+      <main className="bg-gray-100 h-screen"></main>
     </>
   );
 };
